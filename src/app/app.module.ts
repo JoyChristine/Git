088a004cb46b7b3efb5d28defd824f252498ff11
Joy Christine <joychristin2@gmail.com>
2022-04-15 14:33:09 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ClassesComponent } from './classes/classes.component';
+import { SearchComponent } from './search/search.component';
+import { PipePipe } from './pipe/pipe.pipe';
+import { DirectiveDirective } from './directive/directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClassesComponent,
+    SearchComponent,
+    PipePipe,
+    DirectiveDirective
   ],
   imports: [
     BrowserModule,
