4645ba0bdaad14bba643ebf5ad1312c561e20b89
Joy Christine <joychristin2@gmail.com>
2022-04-15 18:31:48 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ClassesComponent } from './classes/classes.component';
+
 import { SearchComponent } from './search/search.component';
 import { PipePipe } from './pipe/pipe.pipe';
 import { DirectiveDirective } from './directive/directive.directive';
@@ -13,7 +13,6 @@ import { DirectiveDirective } from './directive/directive.directive';
   declarations: [
     AppComponent,
     NavbarComponent,
-    ClassesComponent,
     SearchComponent,
     PipePipe,
     DirectiveDirective
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  GitKey: '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'
+ 
 };
 
 /*
