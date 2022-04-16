78895c7d9d1874f040926a847ae6ccd8dfb92da7
Joy Christine <joychristin2@gmail.com>
2022-04-16 10:45:19 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { PipePipe } from './pipe/pipe.pipe';
 import { DirectiveDirective } from './directive/directive.directive';
+import { RepositoriesComponent } from './component/repositories/repositories.component';
+import { UsersComponent } from './component/users/users.component';
+import { HomeComponent } from './component/home/home.component';
+
 
 
 @NgModule({
@@ -17,6 +21,10 @@ import { DirectiveDirective } from './directive/directive.directive';
     SearchComponent,
     PipePipe,
     DirectiveDirective,
+    RepositoriesComponent,
+    UsersComponent,
+    HomeComponent,
+  
     
   ],
   imports: [
