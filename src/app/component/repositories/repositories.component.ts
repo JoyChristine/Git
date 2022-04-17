d875aec8767d3dfd38f317c68a6755c7317899b3
Joy Christine <joychristin2@gmail.com>
2022-04-17 14:01:51 +0300

    add search method

--- src/app/component/repositories/repositories.component.ts
+++ src/app/component/repositories/repositories.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  styleUrls: ['./repositories.component.css'],
+  providers: [DataService]
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  repos:any;
+  username!: string;
+    
+    constructor(private dataService: DataService){
+      console.log('rere')
+      this.dataService.getGitRepos()
+      .subscribe(repos => {
+        // console.log('rerere');
+        (this.repos = repos);
+        
+      });
+
+    }
+    search(){
+      this.dataService.updateUsername(this.username);
+      this.dataService.getGitRepos()
+      .subscribe(repos => {
+        // console.log('rerere');
+        (this.repos = repos);
+        
+      });
+    }
+   
+
+
+
 
   ngOnInit(): void {
   }
