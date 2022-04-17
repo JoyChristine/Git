9f7387527721e29ffcb69e7e2eff51f557739fe0
Joy Christine <joychristin2@gmail.com>
2022-04-17 13:55:01 +0300

    add methods to get repo & users

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,26 +1,96 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment'
-import { map } from 'rxjs/operators';
+
+import { map } from 'rxjs/internal/operators/map';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
- 
-  private userName!: string;
-
-  constructor(private http: HttpClient) {
-    console.log('app is running');
-  }
-  getUserData(){
-    const url = "https://api.github.com/users";
+  updateusername: any;
   
-    return this.http.get(url+this.userName+'?access_token='+environment.key);
-    // .pipe(map((data) => 
-    // { return data; }));
-  }
-  getReposData(){
 
+
+  // private key: string = 'environment.key';
+  private key: string = 'environment.key';
+
+
+  private username!: string;
+
+
+  constructor(private http: HttpClient){
+      console.log('github service is running');
+      this.username = 'JoyChristine';
   }
+
+  getGitUser(){
+    return this.http
+      .get('https://api.github.com/users/'+this.username)
+      .pipe(map((res) => res));
+  }
+  getGitRepos(){
+    return this.http
+      .get('https://api.github.com/users/'+this.username+'/repos')
+      .pipe(map((res) => res));
+  }
+  updateUsername(username:any){
+    this.username = username;
+  }
+  // updateUsername(username:string){
+  //   this.username = username;
+  // }
+
+  
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// private userName!: string;
+
+//   constructor(private http: HttpClient) {
+//     console.log('app is running');
+//   }
+//   getUserData(){
+//     const url = "https://api.github.com/users";
+  
+//     return this.http.get(url+this.userName+'?access_token='+environment.key);
+//     // .pipe(map((data) => 
+//     // { return data; }));
+//   }
+//   getReposData(){
+
+//   }
