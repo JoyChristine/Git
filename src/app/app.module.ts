e07398a4ed8d75fa7d999798723eb8a43f7e1531
Joy Christine <joychristin2@gmail.com>
2022-04-17 14:03:32 +0300

    import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,6 +11,7 @@ import { DirectiveDirective } from './directive/directive.directive';
 import { RepositoriesComponent } from './component/repositories/repositories.component';
 import { UsersComponent } from './component/users/users.component';
 import { HomeComponent } from './component/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -29,7 +30,10 @@ import { HomeComponent } from './component/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
