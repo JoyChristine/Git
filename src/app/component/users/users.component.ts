e94a0bbcb3628c88856bffee0cffe77dee3d9f2c
Joy Christine <joychristin2@gmail.com>
2022-04-17 13:59:20 +0300

    add search method

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,23 +1,71 @@
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [DataService]
 })
 export class UsersComponent implements OnInit {
+  user:any =[];
+  username!: string;
+  
+    
+    constructor(private dataService: DataService){
+      console.log('rere')
+      this.dataService.getGitUser()
+      .subscribe(user => {
+        // console.log('rerere');
+        (this.user = user);
+        
+      });
+      
+     
+      
+    }
+    search(){
+      this.dataService.updateUsername(this.username);
+      this.dataService.getGitUser()
+        .subscribe(user => {
+          console.log('why so serious');
+          (this.user = user);
+          
+        });
+    }
    
-  // users: string[] = []; 
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// users: string[] = []; 
   // data: string[] =[];
   // constructor(private http: HttpClient, private dataService: DataService) { }
 
   // getUsers(){
   //   this.dataService.getUserData().subscribe(_data =>(this.users.data));
   // }
-
-  ngOnInit(): void {
-  }
-
-}
