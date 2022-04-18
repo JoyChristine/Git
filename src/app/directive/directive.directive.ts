07c46ded6614541ebd33fa40d14b079ea4d63369
Joy Christine <joychristin2@gmail.com>
2022-04-19 00:10:18 +0300

    add hover effect on repo name

--- src/app/directive/directive.directive.ts
+++ src/app/directive/directive.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDirective]'
 })
 export class DirectiveDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {}
+  underline = 'white'
+  @HostListener("mouseover") 
+  onMouseOver(){
+    this.textDeco("underline ")
+  }
 
+  @HostListener("mouseout") 
+  onMouseOut(){
+    this.textDeco("None")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+
+  }
+ 
+ 
 }
